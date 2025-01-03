d1decc7be03d18254ec0ce66cf7440c9ee7c3498
bittelo <pedrohanza432@gmail.com>
2025-01-03 19:51:48 -0300

    Projeto praticamente finalizado

--- JogodaVelha/jogodavelha.c
+++ JogodaVelha/jogodavelha.c
@@ -10,10 +10,9 @@ int main() {
     int linha, coluna, posicao;
     int cont = 0;
     char tabuleiro[qtdlinhas][qtdcolunas];
-    char atual =PX;
-    char ganhador = empate; 
+    char atual = PX;
+    char ganhador = empate;
 
-   
     for (linha = 0; linha < qtdlinhas; linha++) {
         for (coluna = 0; coluna < qtdcolunas; coluna++) {
             tabuleiro[linha][coluna] = charp;
@@ -21,7 +20,6 @@ int main() {
     }
 
     while (1) {
-        printf("\n\n%d\n\n", cont);
         printf("\nTabuleiro atual:\n");
         for (linha = 0; linha < qtdlinhas; linha++) {
             for (coluna = 0; coluna < qtdcolunas; coluna++) {
@@ -30,70 +28,62 @@ int main() {
             printf("\n");
         }
 
-        
-        printf("\nJogador %c, faça sua jogada (1-9):\n",atual);
-            scanf("%d", &posicao);
+        printf("\nJogador %c, faça sua jogada (1-9):\n", atual);
+        scanf("%d", &posicao);
 
-        
         if (posicao < 1 || posicao > 9) {
             printf("Posição inválida! Escolha um número entre 1 e 9.\n");
-                continue;
+            continue;
         }
 
-        
         linha = (posicao - 1) / qtdcolunas;
         coluna = (posicao - 1) % qtdcolunas;
 
-        
         if (tabuleiro[linha][coluna] != charp) {
             printf("Essa posição já foi preenchida, escolha outra.\n");
-                continue;
+            continue;
         }
 
-
-        for(linha = 0; linha < qtdlinhas; linha +=1){
-            if(tabuleiro[linha][0] == PX && tabuleiro [linha][1] == PX && tabuleiro [linha][2] == PX){
-            ganhador == PX;
-                break;
-        }
-        
-    }
-
-        for(coluna = 0; coluna < qtdcolunas; coluna +=1){
-            if(tabuleiro[0][coluna] == PX && tabuleiro [0][coluna] == PX && tabuleiro [0][coluna] == PX){
-                ganhador == PX;
-                    break;
-            
-            }   
-        }
-        
-        if(tabuleiro[0][0] == PX && tabuleiro [1][1] == PX && tabuleiro [2][2] == PX){
-            ganhador == PX;
-            break;
-        }
-
-        if(tabuleiro[0][2] == PX && tabuleiro [1][1] == PX && tabuleiro [2][0] == PX){
-            ganhador == PX;
-            break;
-        }
-        
-        
-        
         tabuleiro[linha][coluna] = atual;
 
-        cont += 1;
-            
-        if (cont % 2 == 0){
-            atual = PX;
-        } else {
-            atual = PO;
+        for (linha = 0; linha < qtdlinhas; linha++) {
+            if (tabuleiro[linha][0] == atual && tabuleiro[linha][1] == atual && tabuleiro[linha][2] == atual) {
+                ganhador = atual;
+                break;
+            }
         }
 
-
-        
+        for (coluna = 0; coluna < qtdcolunas; coluna++) {
+            if (tabuleiro[0][coluna] == atual && tabuleiro[1][coluna] == atual && tabuleiro[2][coluna] == atual) {
+                ganhador = atual;
+                break;
+            }
         }
+
+        if (tabuleiro[0][0] == atual && tabuleiro[1][1] == atual && tabuleiro[2][2] == atual) {
+            ganhador = atual;
+            break;
+        }
+
+        if (tabuleiro[0][2] == atual && tabuleiro[1][1] == atual && tabuleiro[2][0] == atual) {
+            ganhador = atual;
+            break;
+        }
+
+        cont++;
+        if (cont == 9) {
+            break;
+        }
+
+        atual = (atual == PX) ? PO : PX;
+    }
+
+    printf("\nFim do jogo!\n");
+    if (ganhador == empate) {
+        printf("O jogo terminou em empate.\n");
+    } else {
+        printf("O jogador %c venceu!\n", ganhador);
+    }
 
     return 0;
-
-    
 }
